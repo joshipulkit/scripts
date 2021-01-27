8e52f424f29a75336c892c829294480a92f19977
pulkit <pulkitjoshi896@gmail.com>
2021-01-27 18:18:31 +0530

    config files

---
+++ config_files/fish/config.fish
@@ -0,0 +1,17 @@
+##########################################################
+############    FISH SHELL CONFIG FILE    ################
+##########################################################
+
+
+### ADDING TO PATH ###
+
+# Add /home/pulkit/bin to PATH
+export PATH="/home/pulkit/bin:$PATH"
+# Add /home/pulkit/.local/bin to PATH
+export PATH="/home/pulkit/.local/bin:$PATH"
+
+# Removes ugly background color from some directories
+export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
+
+# Set vim as the default editor
+set EDITOR vim
---
+++ config_files/fish/functions/lrt.fish
@@ -0,0 +1,3 @@
+function lrt
+    ls -lrth $argv
+end
