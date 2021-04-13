f42e61ed4ca1bf36039a8f7bf5510f082536e0b3
pulkit <pulkitjoshi896@gmail.com>
2021-04-13 11:30:43 +0530

    added fish config

--- config_files/fish/config.fish
+++ config_files/fish/config.fish
@@ -9,6 +9,8 @@
 export PATH="/home/pulkit/bin:$PATH"
 # Add /home/pulkit/.local/bin to PATH
 export PATH="/home/pulkit/.local/bin:$PATH"
+# Add /home/pulkit/packages/psi4conda/bin to PATH
+export PATH="/home/pulkit/packages/psi4conda/bin:$PATH"
 
 # Removes ugly background color from some directories
 export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
